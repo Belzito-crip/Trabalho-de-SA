c694bed9ae0ac5684372b7aa8478f63d9e01c4f7
israel veloso <israel_v_de-jesus@estudante.sesisenai.org.br>
2025-08-29 09:48:04 -0300

    teste, mas acho q n funcionou, to ficando louco

---
+++ APi/api.js
@@ -0,0 +1,3 @@
+const itensEstoque = [
+    {produto: "Caderno", desenho: "capivara", modelo: 1, capa: "capa dura", img: "https://images.tcdn.com.br/img/img_prod/1220230/caderno_espiral_capa_dura_universitario_1_materia_capyclub_80_fls_tilibra_5017_variacao_10419_1_31b9debd8621e4354df8ba4bdcb70d1e.jpg"},
+]
